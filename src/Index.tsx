33a770e2be2e41c447c882614ec7551fc4a49458
PedroOpossum <mason.s.morales2005@gmail.com>
2025-12-27 17:05:12 -0600

    Finally got tailwind to work

---
+++ src/Index.tsx
@@ -0,0 +1,12 @@
+import React, { useContext, useState, useEffect } from "react"
+
+function App() {
+
+  return (
+    <h1 className="text-4xl font-bold text-red-500">
+      Tailwind v4 works
+    </h1>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Pages/Authorization_page'
+import './tailwind.css'
+import App from "./Index"; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
